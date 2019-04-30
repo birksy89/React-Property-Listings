bb29a4f60ceacfcfc49a6445c83725dd3d436aa3
Andrew <andrew@purplecs.com>
2019-04-30 14:09:27 +0100

    Initialise Google Map

--- src/js/GoogleMap.js
+++ src/js/GoogleMap.js
@@ -8,15 +8,30 @@ export default class GoogleMap extends Component {
 
   constructor(props) {
     super(props);
+    // this.mapRef = React.createRef();
     this.state = {
       markers: [],
     };
   }
 
+  componentDidMount() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    // eslint-disable-next-line no-undef
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    // eslint-disable-next-line no-undef
+    const marker = new google.maps.Marker({ position: uluru, map });
+  }
+
   render() {
     return (
       <div className="mapContainer">
-        <div id="map" />
+        <div id="map" ref={this.myRef} />
       </div>
     );
   }

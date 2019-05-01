d706de51c345083f7486c4665a7fd12e52f3cb77
Andrew <andrew@purplecs.com>
2019-05-01 11:33:04 +0100

    moving to dedicated Header / Filter Components

---
+++ src/js/Header.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Filter from './Filter';
+import image from '../images/house-location-pin.svg';
+
+function Header({ filterIsVisible, toggleFilter }) {
+  return (
+    <header className={`${filterIsVisible ? 'filter-is-visible' : ''}`}>
+      <Filter toggleFilter={toggleFilter} />
+      <img alt="" src={image} />
+      <h1>Property Listings</h1>
+      <button
+        type="button"
+        className="btn-filter"
+        onClick={e => toggleFilter(e)}
+      >
+        Filter
+      </button>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  filterIsVisible: PropTypes.bool.isRequired,
+  toggleFilter: PropTypes.func.isRequired,
+};
+
+export default Header;
